5a70d08e9309d95aee11545bb616c6a56db59878
dkto2000 <dkto@ucdavis.edu>
2024-03-06 03:45:41 -0800

    added sequence to everypci

--- everpic/script.js
+++ everpic/script.js
@@ -2,4 +2,35 @@
   'use strict'
   console.log('reading js');
 
+  const body = document.querySelector('body');
+  const p = document.querySelector('p');
+  const bA = document.getElementById("back"); 
+  const cos = document.getElementById("countdown");
+  const rightBlock =  document.getElementById("rightBlocks");
+  const leftBlock =  document.getElementById("leftBlocks");
+
+
+  let counter = 10;
+
+  function countTimeDown() {
+      console.log(`running countTimeDown ${11 - counter} time(s)`);
+      setTimeout(function(){
+          if(counter > 0){
+              counter--;
+              p.textContent = counter;
+              countTimeDown();
+
+          }
+          else{
+            cos.style.display="none";
+            rightBlock.style.display="none";
+            overlay.style.display="none";
+            leftBlock.style.display="none";
+            body.className='defaultP';
+           
+          
+          }
+      }, 1000);
+  }
+  countTimeDown();
 }());
