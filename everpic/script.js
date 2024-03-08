04d2198369793b34437856855d8759c147ed123f
dkto2000 <dkto@ucdavis.edu>
2024-03-08 07:38:55 -0800

    working on responsive

--- everpic/script.js
+++ everpic/script.js
@@ -2,6 +2,8 @@
   'use strict'
   console.log('reading js');
 
+
+
   const body = document.querySelector('body');
   const p = document.querySelector('p');
   const cos = document.getElementById("countdown");
@@ -11,6 +13,8 @@
 
   let counter = 10;
 
+
+  // function to call on timer to load main content in.
   function countTimeDown() {
       console.log(`running countTimeDown ${11 - counter} time(s)`);
       setTimeout(function(){
@@ -18,9 +22,9 @@
               counter--;
               p.textContent = counter;
               countTimeDown();
-
-          }
-          else{
+// Makes countdown disappear and brings in next page
+          } 
+          else{  
             reel.style.display="none";
             real.style.display="block";
             body.id="defaultP";
@@ -30,4 +34,73 @@
       }, 1000);
   }
   countTimeDown();
+
+
+window.addEventListener('load', function () {
+
+	const posts = document.querySelectorAll('section');
+	const figureImg = document.querySelector('figure img');
+	let postTops = [];
+	let pageTop;
+	let counter = 1;
+	let prevCounter = 1;
+	let doneResizing;
+
+	
+	resetPagePosition();
+
+
+	window.addEventListener('scroll', function () {
+		pageTop = window.pageYOffset + 400;
+
+
+		// if the page is being scrolled
+		if (pageTop > postTops[counter]) {
+			counter++;
+			
+		}
+		// if page is being scrolled up
+		else if (counter > 1 && pageTop < postTops[counter - 1]) {
+			counter--;
+		
+		}
+
+		// Every time a section changes
+		if (counter != prevCounter) {
+			
+			figureImg.className = 'map' + counter;
+			prevCounter = counter;
+		}
+
+	}); // end window scroll function
+
+	// Resize window event
+	window.addEventListener('resize', function () {
+
+		clearTimeout(doneResizing);
+		// this runs after the window has stopped being resized...
+		doneResizing = setTimeout(function () {
+
+			resetPagePosition();
+
+		}, 500);
+	});
+
+	// this function resets variables that may have been altered after resizing
+	function resetPagePosition() {
+		postTops = [];
+		posts.forEach(function (post) {
+			postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
+		});
+
+		const pagePosition = window.pageYOffset + 300;
+		counter = 0;
+
+		postTops.forEach(function (post) { if (pagePosition > post) { counter++; } });
+
+	}
+
+});
+
+
 }());
